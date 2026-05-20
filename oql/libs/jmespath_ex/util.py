ecade8347165a4e1136b9596b18594b9d0c565f2
Chris.King <chrisking941@outlook.com>
2026-05-20 16:07:46 +0800

    1. Fix jmespath field extraction

--- oql/libs/jmespath_ex/util.py
+++ oql/libs/jmespath_ex/util.py
@@ -10,17 +10,65 @@ __all__ = ["extract_fields"]
 class FullPathExtractor(visitor.Visitor):
     def __init__(self):
         self.found_paths = set()
+        self._prefix_stack = []  # Stack to track array/object prefixes
 
     def visit(self, node, *args, **kwargs):
+        # Handle projection nodes (array projections like order_lines[].field)
+        if node['type'] == 'projection':
+            # First child can be a field or flatten node
+            array_child = node['children'][0]
+            prefix_added = False
+            
+            # Extract the array field name
+            if array_child['type'] == 'field':
+                # Direct field reference
+                self._prefix_stack.append(array_child['value'])
+                prefix_added = True
+            elif array_child['type'] == 'flatten':
+                # Flatten node contains the actual field
+                flatten_child = array_child['children'][0]
+                if flatten_child['type'] == 'field':
+                    self._prefix_stack.append(flatten_child['value'])
+                    prefix_added = True
+            
+            # Visit all children with the prefix context
+            for child in node.get('children', []):
+                self.visit(child)
+            
+            # Pop the prefix after processing
+            if prefix_added:
+                self._prefix_stack.pop()
+            return self.found_paths
+        
+        # Handle key_val_pair nodes (inside multi_select_dict)
+        # key_val_pair has structure: {type: 'key_val_pair', value: 'key_name', children: [expression_node]}
+        # We need to visit the child expression to extract fields from it
+        if node['type'] == 'key_val_pair':
+            # Visit the child expression (the value part of key: value)
+            for child in node.get('children', []):
+                self.visit(child)
+            return self.found_paths
+        
         # A subexpression is a chain like 'partner_id.name'
         if node['type'] == 'subexpression':
             path = self._resolve_chain(node)
             if path:
-                self.found_paths.add(path)
+                # Add current prefix if exists
+                if self._prefix_stack:
+                    full_path = ".".join(self._prefix_stack) + "." + path
+                    self.found_paths.add(full_path)
+                else:
+                    self.found_paths.add(path)
 
         # A field is a standalone path like 'name'
         elif node['type'] == 'field':
-            self.found_paths.add(node['value'])
+            field_name = node['value']
+            # Add current prefix if exists
+            if self._prefix_stack:
+                full_path = ".".join(self._prefix_stack) + "." + field_name
+                self.found_paths.add(full_path)
+            else:
+                self.found_paths.add(field_name)
 
         # Recursively check children for other expressions (like inside {key: path})
         for child in node.get('children', []):
