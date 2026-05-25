74abbd5dc665255d53fa8fac6cc065bc794bc375
Chris.King <chrisking941@outlook.com>
2026-05-25 18:11:39 +0800

    Release oql version 19.0.1.5.32

--- oql/__manifest__.py
+++ oql/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OQL - Odoo Query Language",
-    "version":"1.5.31",
+    "version":"19.0.1.5.32",
     "author": "Chris",
     "website": "https://github.com/chrisking94/odoo_addons/tree/main/oql",
     "license": "LGPL-3",
