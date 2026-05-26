ccfe56596f9139f2f2ca94b862c2468b2319f4a7
Chris.King <chrisking941@outlook.com>
2026-05-26 23:47:42 +0800

    Release oql_web version 1.5.0

--- oql_web/__manifest__.py
+++ oql_web/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OQL Web",
-    "version":"1.4.2",
+    "version":"1.5.0",
     "author": "Chris",
     "website": "https://github.com/chrisking94/odoo_addons/tree/main/oql_web",
     "license": "LGPL-3",
