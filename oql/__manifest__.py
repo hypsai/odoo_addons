03da540e64ba01f05fb88d846053c2bc7f769bf4
Chris.King <chrisking941@outlook.com>
2026-05-30 19:38:23 +0800

    Release oql version 1.9.14

--- oql/__manifest__.py
+++ oql/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OQL - Odoo Query Language",
-    "version":"1.9.13",
+    "version":"1.9.14",
     "author": "Hypsai Tech",
     "website": "https://github.com/hypsai/odoo_addons/tree/main/oql",
     "license": "LGPL-3",
