26431c90cb5e06d83179015156bc7bc7496b2d8c
Chris.King <chrisking941@outlook.com>
2026-07-16 15:42:27 +0800

    Release attached_field v1.0.5 for Odoo 15.0 [skip ci]

--- attached_field/__manifest__.py
+++ attached_field/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attached Field",
-    "version":"15.0.1.0.4",
+    "version":"15.0.1.0.5",
     "category": "Tools",
     "summary": "Dynamically attach fields to models from action methods.",
     "description": """
