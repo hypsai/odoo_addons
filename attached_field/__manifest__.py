be96449b962d8afc9a1c8d472646d1b264d84094
Chris.King <chrisking941@outlook.com>
2026-07-16 14:21:03 +0800

    Release attached_field version 1.0.3

--- attached_field/__manifest__.py
+++ attached_field/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attached Field",
-    "version":"1.0.2",
+    "version":"1.0.3",
     "category": "Tools",
     "summary": "Dynamically attach fields to models from action methods.",
     "description": """
